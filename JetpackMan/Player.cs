7bd518aac04b168573fbfbf33938d1a18d67d198
Darthfett <darthfett@gmail.com>
2016-04-04 20:10:39 -0700

    Initial Commit: draw img to screen

---
+++ JetpackMan/Player.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JetpackMan
+{
+    class Player
+    {
+        public Vector2 position;
+        public Texture2D texture;
+
+        public Player(Vector2 position, Texture2D texture)
+        {
+            this.position = position;
+            this.texture = texture;
+        }
+    }
+}
